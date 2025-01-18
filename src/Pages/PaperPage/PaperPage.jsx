e8f7121e39a11345123ba3192425b1b6447be1b7
mharoon-dev <haroon.aghani@gmail.com>
2025-01-18 19:49:48 +0500

    chnages

--- src/Pages/PaperPage/PaperPage.jsx
+++ src/Pages/PaperPage/PaperPage.jsx
@@ -1,36 +1,7 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 
-const papers = [
-    {
-      id: 1,
-      name: "Paper 1",
-      books: [
-        { name: "Book 1", pdfs: ["book1_part1.pdf", "book1_part2.pdf"] },
-        { name: "Book 2", pdfs: ["book2_part1.pdf", "book2_part2.pdf"] }
-      ]
-    },
-    {
-      id: 2,
-      name: "Paper 2",
-      books: [
-        { name: "Book 1", pdfs: ["book1_part1.pdf"] },
-        { name: "Book 2", pdfs: ["book2_part1.pdf"] },
-        { name: "Book 3", pdfs: ["book3_part1.pdf"] }
-      ]
-    },
-    {
-      id: 3,
-      name: "Paper 3",
-      books: [
-        { name: "Book 1", pdfs: ["book1_part1.pdf"] }
-      ]
-    },
-    // Add other papers similarly
-  ];
-  
-
-function PaperPage() {
+function PaperPage({ papers }) {
   const { id } = useParams();
   const paper = papers.find((p) => p.id === parseInt(id));
 
